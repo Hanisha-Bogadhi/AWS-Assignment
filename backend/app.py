caa8db243e143a005b71b640ff334e9e5b449f04
Hanisha Bogadhi <hanishabogadhi@gmail.com>
2026-04-25 11:50:01 +0530

    Reset repo with changes

--- backend/app.py
+++ backend/app.py
@@ -4,14 +4,18 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def home():
+    return {"message": "Backend is running"}
+
 @app.route('/submit', methods=['POST'])
 def submit():
     data = request.get_json()
     print("Received:", data)
 
     return jsonify({
-        "message": f"Received {data['name']} with email {data['email']}"
-    })
+    "message": f"Received {data.get('name')} with email {data.get('email')}"
+})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
